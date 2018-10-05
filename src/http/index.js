0bf32a469a2d333745a01a7c181dbd056cb0abe2
RobertBo <bojianye@gmail.com>
2018-10-05 20:45:22 +1300

    removed unused code

--- src/http/index.js
+++ src/http/index.js
@@ -5,24 +5,6 @@ const instance = axios.create({
     timeout: 3000,
 });
 
-//请求拦截处理
-instance.interceptors.request.use(function (config) {
-    // 在发送请求之前做些什么
-    return config;
-}, function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-});
-
-//返回拦截处理
-instance.interceptors.response.use(function (response) {
-    // 对响应数据做点什么
-    return response;
-}, function (error) {
-    // 对响应错误做点什么
-    return Promise.reject(error);
-});
-
 
 export const Post = async (api, params) => {
     return new Promise((resolve, reject) => {
