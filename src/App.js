8f1f05a9b0754c70e661fb5218dfbe7661b4bfe7
RobertBo <bojianye@gmail.com>
2018-10-05 22:36:03 +1300

    removed div from APP.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import Header from './common/header';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './pages/home';
 import Detail from './pages/detail/loadable'
-import store  from './store/'
+import store from './store/'
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <div>
-                    <Header/>
-                    <BrowserRouter>
-                        <div>
-                            <Route path='/' exact component={Home}/>
-                            <Route path='/detail/:id' exact component={Detail}/>
-                        </div>
-                    </BrowserRouter>
-                </div>
+                <Header/>
+                <BrowserRouter>
+                    <div>
+                        <Route path='/' exact component={Home}/>
+                        <Route path='/detail/:id' exact component={Detail}/>
+                    </div>
+                </BrowserRouter>
             </Provider>
         );
     }
